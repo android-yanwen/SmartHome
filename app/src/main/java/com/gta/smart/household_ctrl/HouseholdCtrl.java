758f19a8277136d64150644d320e5c903fb98b27
android-yanwen <314625433@qq.com>
2016-06-01 18:23:56 +0800

    实现了onTaskStart和onTaskFinish两个接口，显示ProgressDialog，当网路访问完成跳转到红外控制界面

--- app/src/main/java/com/gta/smart/household_ctrl/HouseholdCtrl.java
+++ app/src/main/java/com/gta/smart/household_ctrl/HouseholdCtrl.java
@@ -1,5 +1,7 @@
 package com.gta.smart.household_ctrl;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -11,6 +13,8 @@ import android.view.animation.AnimationUtils;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 
+import com.google.gson.Gson;
+import com.gta.smart.httputility.InternetRequest;
 import com.gta.smart.smarthome.R;
 
 /**
@@ -18,10 +22,35 @@ import com.gta.smart.smarthome.R;
  */
 public class HouseholdCtrl extends AppCompatActivity implements View.OnClickListener {
     private RelativeLayout infrared_ly;
+    private ProgressDialog mProgressDialog ;
+    private Context context;
     @Override
     public void onClick(View v) {
-        startActivity(new Intent(HouseholdCtrl.this, InfraredCtrl.class));
-        HouseholdCtrl.this.overridePendingTransition(R.anim.translate, R.anim.windowout);
+
+        InternetRequest internetRequest = new InternetRequest(HouseholdCtrl.this);
+        internetRequest.setOnAsyncTaskListener(new InternetRequest.OnAsyncTaskListener() {
+            @Override
+            public void onTaskStart() {
+                mProgressDialog = new ProgressDialog(context);
+                mProgressDialog.setTitle("Request remote data");
+                mProgressDialog.setMessage("Loading.....");
+                mProgressDialog.setIndeterminate(false);
+                mProgressDialog.setCancelable(false);
+                mProgressDialog.show();
+            }
+
+            @Override
+            public void onTaskFinish(String result) {
+                mProgressDialog.dismiss();
+                startActivity(new Intent(HouseholdCtrl.this, InfraredCtrl.class));
+                HouseholdCtrl.this.overridePendingTransition(R.anim.translate, R.anim.windowout);
+            }
+        });
+        InternetRequest.RequestParamsBean bean = internetRequest.new RequestParamsBean();
+        bean.username = "13040872678";
+        String params = new Gson().toJson(bean);
+        internetRequest.new RequestUserID().execute(params);  //获取AccessToken和UserId
+
     }
 
     @Override
@@ -31,6 +60,7 @@ public class HouseholdCtrl extends AppCompatActivity implements View.OnClickList
         /*Animation animation = AnimationUtils.loadAnimation(HouseholdCtrl.this, R.anim.translate);
         view.startAnimation(animation);*/
         setContentView(view);
+        context = this;
         infrared_ly = (RelativeLayout) view.findViewById(R.id.infrared_ly);
         infrared_ly.setOnClickListener(this);
 

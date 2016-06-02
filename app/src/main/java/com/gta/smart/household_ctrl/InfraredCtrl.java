460c0e25b3db615c53f3006d4a8598369168d8ca
android-yanwen <314625433@qq.com>
2016-06-02 19:16:55 +0800

    添加了smart_on_of_relativelayout的长按响应，修改智能开关名称

--- app/src/main/java/com/gta/smart/household_ctrl/InfraredCtrl.java
+++ app/src/main/java/com/gta/smart/household_ctrl/InfraredCtrl.java
@@ -1,5 +1,8 @@
 package com.gta.smart.household_ctrl;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -9,10 +12,15 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.gta.smart.httputility.HttpUtility;
 import com.gta.smart.httputility.InternetRequest;
+import com.gta.smart.slideswitch.SlideSwitch;
+import com.gta.smart.smarthome.ImageButtonWithText;
 import com.gta.smart.smarthome.R;
 
 /**
@@ -20,14 +28,48 @@ import com.gta.smart.smarthome.R;
  */
 public class InfraredCtrl extends AppCompatActivity {
     private String tag;
+    private RelativeLayout smart_on_of_relativelayout;
+    private TextView on_off_name;
+//    private ImageButtonWithText id_tv;
+    private Context context;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.infrared_ctrl_layout);
+        context = this;
         tag = getPackageName();
         ActionBar actionBar = InfraredCtrl.this.getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+        initView();
 
+    }
+
+    private void initView() {
+        smart_on_of_relativelayout = (RelativeLayout) findViewById(R.id.smart_on_of_relativelayout);
+        // 长按布局修改名称
+        smart_on_of_relativelayout.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Log.i(tag, "smart_on_off_relativelayout long clicked");
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("输入名称");
+                View view = getLayoutInflater().inflate(R.layout.edit_name_dialog, null);
+                final EditText name_et = (EditText) view.findViewById(R.id.input_name_et);
+                name_et.setHint(on_off_name.getText().toString());
+                builder.setView(view);
+                builder.setCancelable(true);
+                builder.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        on_off_name.setText(name_et.getText().toString());
+                    }
+                });
+                builder.setPositiveButton("Cancel", null);
+                builder.show();
+                return true; //返回false无震动，true有震动
+            }
+        });
+        on_off_name = (TextView) findViewById(R.id.on_off_name);
     }
 
     public void onClicked(View view) {
@@ -44,9 +86,6 @@ public class InfraredCtrl extends AppCompatActivity {
         }
     }
 
-//    class RequestParamsBean{
-//        String username;
-//    }
     @Override
     protected void onResume() {
         super.onResume();
@@ -72,4 +111,5 @@ public class InfraredCtrl extends AppCompatActivity {
         }
         return super.onKeyDown(keyCode, event);
     }
+
 }

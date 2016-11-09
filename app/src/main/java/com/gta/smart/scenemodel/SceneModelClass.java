9fbcefc5c245ffe656fc454cf7cd07c31bf071d1
314625433@qq.com <314625433@qq.com>
2016-11-09 14:48:12 +0800

    .

--- app/src/main/java/com/gta/smart/scenemodel/SceneModelClass.java
+++ app/src/main/java/com/gta/smart/scenemodel/SceneModelClass.java
@@ -20,26 +20,19 @@ import com.handmark.pulltorefresh.library.PullToRefreshScrollView;
 /**
  * Created by Administrator on 2016/6/12.
  */
-<<<<<<< HEAD
-public class SceneModelClass extends AppCompatActivity implements View.OnClickListener{
-=======
-public class SceneModelClass extends AppCompatActivity {
->>>>>>> 88b2e8ee06048ae2a1c5ae91357888376b893436
+public class SceneModelClass extends AppCompatActivity{
     private ImageButtonWithText id_leave_home_btn;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scene_model_layout);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-<<<<<<< HEAD
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
-=======
->>>>>>> 88b2e8ee06048ae2a1c5ae91357888376b893436
     }
 
     @Override
@@ -52,20 +45,6 @@ public class SceneModelClass extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-<<<<<<< HEAD
     public void onClicked(View view) {
     }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.scene_return_home_btn:
-
-                break;
-            case R.id.scene_leave_home_btn:
-                break;
-        }
-    }
-=======
->>>>>>> 88b2e8ee06048ae2a1c5ae91357888376b893436
 }
